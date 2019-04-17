dd709432cc65278a3c9003dbc51b0ac000349272
AJ Savino <aj.savino@moxieusa.com>
2019-04-17 16:39:52 -0400

    updated index.js to match readme

--- index.js
+++ index.js
@@ -2,40 +2,39 @@ const RemoteMerge = require("./remote-merge.js");
 
 (async () => {
 	try {
-		/*var originalSnapshot = await RemoteMerge.snapshot("./test/a", /^.+\.txt$/);
-		//console.log("original:", originalSnapshot);
-		//await RemoteMerge.save("original-snapshot.json", originalSnapshot);
-
-		var modifiedSnapshot = await RemoteMerge.snapshot("./test/b", /^.+\.txt$/);
-		//console.log("modified:", modifiedSnapshot);
-		//await RemoteMerge.save("modified-snapshot.json", modifiedSnapshot);
-
-		var comparison = RemoteMerge.compare(originalSnapshot, modifiedSnapshot);
-		console.log(comparison);
-		console.log();
-
-		var zip = await RemoteMerge.generatePackage("./test/b", comparison);
-		//console.log(zip);
-		await RemoteMerge.saveZip(zip);*/
-
-		//Build package
+		//Simple API
+		
+		//Build package comparing original content in 'a' to modified content in 'b'
 		var packageName = await RemoteMerge.generateMergePackage("./test/a", "./test/b");
-
-		//Apply package
+		//Apply package containing what changed to our original content
 		await RemoteMerge.applyMergePackage("./test/a", packageName);
 
-		//Make sure to create a 'c' folder
-		//RemoteMerge.zipToDir((await RemoteMerge.loadZip("remote-merge_1555459586939.zip")), "./test/c");
+		//Advanced API
+		/*
+		//Take a snapshot of a folder
+		var originalSnapshot = await RemoteMerge.snapshot("./test/a"); //Note you can specify a file filter and hashing algorithm (MD5 by default)
+		await RemoteMerge.save("original-snapshot.json", originalSnapshot);
+
+		//Take a snapshot of the modified folder
+		var modifiedSnapshot = await RemoteMerge.snapshot("./test/b"); //Note you can specify a file filter and hashing algorithm (MD5 by default)
+		await RemoteMerge.save("modified-snapshot.json", modifiedSnapshot);
+
+		//Compare snapshots
+		var originalSnapshot = await RemoteMerge.load("original-snapshot.json");
+		var modifiedSnapshot = await RemoteMerge.load("modified-snapshot.json");
+		var comparison = RemoteMerge.compare(originalSnapshot, modifiedSnapshot);
+
+		//Generate package
+		//Our package containing what changed comes from our modified content
+		var zip = await RemoteMerge.generatePackage("./test/b", comparison);
+		await RemoteMerge.saveZip(zip, "./package.zip");
+
+		//Apply package
+		//Our package containing what changed gets applied to the original content
+		var zip = await RemoteMerge.loadZip("package.zip");
+		await RemoteMerge.applyPackage("./test/b", zip); //Note that the zip filename is optional. If not specified one will be generated.
+		*/
 	} catch (err){
 		console.error("CAUGHT ERROR:", err);
 	}
-})()
-
-//var modifiedSnapshot = RemoteMerge.snapshot("./test/b");
-
-//RemoteMerge.diff(originalSnapshot, modifiedSnapshot);
-
-//TODO: Build package
-//TODO: Install package
-
-console.log();
+})();
